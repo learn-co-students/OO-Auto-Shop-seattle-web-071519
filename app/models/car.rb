a9504fdf65db5053de79144e4bed44b807ed1850
ayerest <irisiseve@gmail.com>
2019-07-21 15:13:25 -0700

    finished OO Auto Shop lab

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,37 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :owner, :mechanic, :classification
+  @@all = []
 
-  def initialize(make, model, classification)
+  #adding owner and mechanic at initialization so I can relate
+  #cars to owners and cars to mechanics
+  def initialize(make, model, classification, owner, mechanic)
     @make = make
     @model = model
     @classification = classification
+    @owner = owner
+    @mechanic = mechanic
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.list_classifications
+    self.all.collect do |car|
+      car.classification
+    end
+    #duplicates show up, could do uniq to have each 
+    #classication listed once or turn into a hash with counts for
+    #how many cars have each classification
+  end
+
+  def mechanics
+    #get a list of mechanics that specialize in this car classification
+    Mechanic.all.select do |mechanic|
+      mechanic.specialty == self.classification
+    end
   end
 
 end
--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,9 +1,44 @@
 class CarOwner
 
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def cars
+    #get a list of all the cars owned
+    Car.all.select do |car|
+      car.owner == self
+    end
+  end
+
+  #helper method to get number of cars
+  def car_count
+    self.cars.length
+  end
+
+  def mechanics
+    #get a list of mechanics used
+    #call self.cars and iterate through that list to
+    #collect the mechanic attached to each car
+    self.cars.collect do |car|
+      car.mechanic
+    end.uniq
+  end
+
+  def self.average_cars_owned
+    #find out the average number of cars owned for all owners
+    sum_cars = self.all.map do |owner|
+      owner.car_count
+    end.sum
+    sum_cars / (self.all.length * 1.0)
   end
 
 
